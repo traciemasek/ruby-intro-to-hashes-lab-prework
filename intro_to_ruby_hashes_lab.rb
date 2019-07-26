05990038f13301510092b7583e9bf91a21ac33cf
Tracie Masek <traciemasek@gmail.com>
2019-07-26 17:35:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,7 +48,10 @@ def monopoly_with_fourth_tier
 	  railroads: {
 	    pieces: 4,
 	    names: {
-	      reading_railroad: {mortgage_value: "$100"},
+	      reading_railroad: {
+	        mortgage_value: "$100"
+	        
+	      },
 	      pennsylvania_railroad: {mortgage_value: "$200"},
 	      b_and_o_railroad: {mortgage_value: "$300"},
 	      shortline: {mortgage_value: "$400"}
